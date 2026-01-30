b5fe8aae6b0757db2866f6cb18bfb0ee19e3c26d
AnnaChason <annachason.wiggins@my.wheaton.edu>
2026-01-30 10:02:19 -0600

    improved shelf label readability

--- src/components/Shelf.tsx
+++ src/components/Shelf.tsx
@@ -11,8 +11,8 @@ const Shelf: React.FC<ShelfProps> = ({children, category}) => {
             <div className={" max-w-screen-lg flex flex-wrap justify-center items-end"}>
                 {children}
                 <div className={" w-full h-10 rounded bg-woodTexture bg-contain mb-5 border border-brown-400 flex justify-center"}>
-                    <div className={'bg-woodPlaque  bg-[length:100%_100%] bg-no-repeat bg-center h-10 w-fit min-w-24 text-center text-sm p-2 pr-8 pl-8 font-bold text-brown-800 '}>
-                        <h3 className={'translate-y-0.5'}>{category}</h3>
+                    <div className={'bg-woodPlaque  bg-[length:100%_100%] bg-no-repeat bg-center h-10 w-fit min-w-24 text-center text-lg p-2 pr-8 pl-8 font-bold text-brown-800 flex flex-col justify-center'}>
+                        <h3 className={category.length > 10 ? "ml-4 mr-4" : ""}>{category}</h3>
                     </div>
                 </div>
             </div>
